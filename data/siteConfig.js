135f02c0418bdddf0a787e61e8c861bd5e1a8101
kavimihiranga20 <kmvithanage48@gmail.com>
2021-07-11 22:53:23 +0530

    frist commit

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-  siteTitle: 'Hi! I\'m Francisco!',
-  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  siteTitle: 'Hi! I\'m Kavindu!',
   keyWords: ['gatsbyjs', 'react', 'curriculum'],
-  authorName: 'Francisco Santos',
-  twitterUsername: '_franciscodf',
-  githubUsername: 'santosfrancisco',
-  authorAvatar: '/images/avatar.jpeg',
-  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
-    In 2012 I started working as a support technician and I approached the developers.
-    In 2015, I started to study C # and started to contribute with the team giving maintenance in an application in C # and .NET. <br />
-    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  authorName: 'Kavindu Mihiranga',
+  githubUsername: 'Kavimihiranga20',
+  authorAvatar: '/images/me.jpg',
+  authorDescription: `I am an ambitious and positive person who enjoys taking up challenges in the field of Software Engineering. I am passionate about combining the knowledge of programming with my analytical skills. I always commit myself to continuous improvement and believe in endless opportunities. I am also a friendly and a polite person who enjoys team working.`,
   skills: [
+    {
+      name: 'JAVA',
+      level: 60
+    },
+    
     {
       name: 'HTML',
       level: 70
@@ -29,7 +29,7 @@ module.exports = {
     },
     {
       name: 'React',
-      level: 60
+      level: 45
     },
     {
       name: 'Git',
@@ -40,45 +40,16 @@ module.exports = {
   jobs: [
     /* more jobs here */
     {
-      company: "Gympass",
+      company: "Axcer",
       begin: {
-        month: 'sep',
-        year: '2019'
+        month: 'May',
+        year: '2021'
       },
       duration: null,
-      occupation: "Frontend developer",
-      description: "I am part of the Corporate team, responsible for the development and maintenance of the employee management platform, giving more and more autonomy to partner companies."
+      occupation: " Developer",
+      description: "Doing my internship"
   
-    },  {
-      company: "Lendico",
-      begin: {
-        month: 'apr',
-        year: '2018'
-      },
-      duration: null,
-      occupation: "Frontend developer",
-      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
-  
-    }, {
-      company: "Anapro",
-      begin: {
-        month: 'dec',
-        year: '2016'
-      },
-      duration: '1 yr e 5 mos',
-      occupation: "Fullstack developer",
-      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
-    }, {
-      company: "Anapro",
-      begin: {
-        month: 'set',
-        year: '2012'
-      },
-      duration: '4 yrs e 3 mos',
-      occupation: "Support Technician",
-      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
-    },
+    },  
   ],
   portifolio: [
     {
@@ -94,13 +65,13 @@ module.exports = {
     /* more portifolio items here */
   ],
   social: {
-    twitter: "https://twitter.com/_franciscodf",
-    linkedin: "https://www.linkedin.com/in/santos-francisco",
-    github: "https://github.com/santosfrancisco",
-    email: "yoshi.df@gmail.com"
+    StackOverflow:"https://stackoverflow.com/users/16173690/kavindu-mihiranga",
+    linkedin: "https://www.linkedin.com/in/kavindu-mihiranga-1600a01b6",
+    github: "https://github.com/Kavimihiranga20",
+    email: "kmvithanage48@gmail.com"
   },
-  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
-  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  
+  pathPrefix: '/cv-site', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
   googleAnalyticsId: 'UA-000000000-1',
   background_color: '#ffffff',
